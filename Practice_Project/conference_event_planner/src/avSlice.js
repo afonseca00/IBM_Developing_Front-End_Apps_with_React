efeb65bb6a481adcfab16f01a8255f234024e6df
afonseca00 <113251401+afonseca00@users.noreply.github.com>
2024-11-27 11:55:28 +0000

    Final Practice_Project

--- Practice_Project/conference_event_planner/src/avSlice.js
+++ Practice_Project/conference_event_planner/src/avSlice.js
@@ -3,16 +3,53 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+        {
+        img: "https://cdn.pixabay.com/photo/2022/02/20/02/29/projector-7023604_1280.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2016/11/14/04/42/techland-1822627_1280.jpg",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2016/01/24/23/03/microphone-1159791_1280.jpg",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2017/10/30/19/24/whiteboard-2903269_1280.png",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+
+    {
+        img: "https://pixabay.com/get/g1412a380d0b370d5e3172bdb530a934a74c1f8426573ad96824c2010ae6a3f35c24e9ec815f2409aa44dd78abf3d5b22501eed839abf258a34100e05fad72325d93a6e9e1ac44419918fc89c8d820be1_640.jpg",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
+
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
